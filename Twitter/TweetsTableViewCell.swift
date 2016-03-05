914454184ed3381f445525f283c2e3753d58ae9e
Eileen Madrigal <eileenmadrigal@Eileens-Mac.local>
2016-03-05 00:00:38 -0600

    Here is my commit message

---
+++ Twitter/TweetsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetsTableViewCell.swift
+//  Twitter
+//
+//  Created by Eileen Madrigal on 2/21/16.
+//  Copyright © 2016 Eileen Madrigal. All rights reserved.
+//
+
+import UIKit
+
+class TweetsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
